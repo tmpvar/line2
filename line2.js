9468858e38359c6ddc61a3eb05d714aaa705725b
Elijah Insua <tmpvar@gmail.com>
2014-02-07 06:31:32 -0700

    first round implementation

--- line2.js
+++ line2.js
@@ -1,14 +1,177 @@
-var line2 = function() {
-
+if (typeof require !== 'undefined') {
+  var Vec2 = require('vec2');
 }
+
+var isArray = function (a) {
+  return Object.prototype.toString.call(a) === "[object Array]";
+};
+
+var defined = function(a) {
+  return typeof a !== 'undefined';
+}
+
+var finite = function(a) {
+  return a !== Infinity && a !== -Infinity;
+};
+
+
+function Line2(slope, yintercept, x2, y2) {
+
+  if (!(this instanceof Line2)) {
+    return new Line2(slope, yintercept);
+  }
+
+  if (defined(x2) && defined(y2)) {
+    return Line2.fromPoints(slope, yintercept, x2, y2);
+
+  } else {
+    if (defined(slope)) {
+      this.slope(slope);
+    }
+
+    if (defined(yintercept)) {
+      this.yintercept(yintercept);
+    }
+  }
+}
+
+Line2.prototype.yintercept = function(val) {
+
+  if (val) {
+    if (finite(val)) {
+      val = Vec2.clean(val);
+    }
+
+    this._yintercept = val;
+  }
+  return this._yintercept || 0;
+}
+
+Line2.prototype.xintercept = function(val) {
+
+  if (val) {
+    if (finite(val)) {
+      val = Vec2.clean(val);
+    }
+
+    this._xintercept = val;
+  }
+  return this._xintercept || 0;
+}
+
+Line2.prototype.slope = function(val) {
+  if (val) {
+    if (finite(val)) {
+      val = Vec2.clean(val);
+    }
+
+    this._slope = val;
+  }
+  return this._slope || 0;
+};
+
+Line2.prototype.intersect = function(line) {
+
+  var s1 = this.slope();
+  var s2 = line.slope();
+
+  // Parallel lines
+  if (s1 === s2) {
+    return false;
+  }
+
+  var fs1 = finite(s1);
+  var fs2 = finite(s2);
+  var yi1 = this.yintercept();
+  var yi2 = line.yintercept();
+  var x;
+
+  if (fs1 && fs2) {
+    var yi = yi1 + yi2;
+    var si = s1 - s2;
+    x = yi/si;
+    return Vec2(x, s1*x + yi1);
+
+  } else {
+    var slope, yi, x = this.xintercept() || line.xintercept();
+    if (!fs1) {
+      slope = s2;
+      yi = yi2;
+    } else {
+      slope = s1;
+      yi = yi1;
+    }
+
+    // Diagonal line
+    if (slope !== 0) {
+      return Vec2(x, x*slope + yi);
+
+    // Horizonal line
+    } else {
+      return Vec2(x, yi);
+    }
+  }
+};
+
+Line2.fromPoints = function(x1, y1, x2, y2) {
+
+  if (isArray(y1)) {
+    y2 = y1[1];
+    x2 = y1[0];
+  } else if (defined(y1) && defined(y1.x) && defined(y1.y)) {
+    y2 = y1.y;
+    x2 = y1.x;
+  }
+
+  if (isArray(x1)) {
+    y1 = x1[1];
+    x1 = x1[0];
+  } else if (defined(x1) && defined(x1.x) && defined(x1.y)) {
+    y1 = x1.y;
+    x1 = x1.x;
+  }
+
+  var line = new Line2();
+  var slope = (y2 - y1) / (x2 - x1);
+
+  line.slope(slope);
+  line.yintercept(y1 - line.slope() * x1);
+
+  if (!finite(slope)) {
+    line.xintercept(x2);
+  }
+
+  return line;
+};
+
+Line2.prototype.isHorizontal = function() {
+  return this.slope() === 0;
+};
+
+Line2.prototype.isVertical = function() {
+  return !finite(this.slope());
+};
+
+Line2.prototype.closestPointTo = function(vec) {
+  var yi = this.yintercept();
+  var xi = this.xintercept();
+  var s = this.slope();
+
+  if (this.isHorizontal()) {
+    return Vec2(vec.x, yi);
+  } else if (this.isVertical()) {
+    return Vec2(xi, vec.y);
+  } else {
+    var line = Line2.fromPoints(vec.x, vec.y, vec.x+s, vec.y-s);
+    return this.intersect(line);
+  }
+};
 
 
 if (typeof module !== "undefined" && typeof module.exports == "object") {
-  module.exports = line2;
+  module.exports = Line2;
 }
 
 if (typeof window !== "undefined") {
   window.line2 = window.line2 || line2;
 }
-
-module.exports = Line2
--- test/test.js
+++ test/test.js
@@ -1,16 +1,107 @@
-var line2;
 if (typeof require !== "undefined") {
-  line2 = require("../line2.js");
-} else {
-  line2 = window.line2;
+  var Line2 = require("../line2.js");
+  var Vec2 = require("vec2");
 }
 
 var ok = function(a, msg) { if (!a) throw new Error(msg || "not ok"); };
 
 describe('line2', function() {
-  describe('#', function() {
-    it('', function() {
-      
+  describe('#ctor', function() {
+    it('accepts slope/yintercept', function() {
+      var l = new Line2(1, 0);
+      ok(l.slope() === 1);
+      ok(l.yintercept() === 0);
+    });
+
+    it('accepts 4 points', function() {
+      var l = new Line2(0, 0, 10, 10);
+      ok(l.slope() === 1);
+      ok(l.yintercept() === 0);
+    });
+  })
+
+
+  describe('#fromPoints', function() {
+    it('computes the slope and yintercept', function() {
+      [
+        Line2.fromPoints([1, 1], [10, 10]),
+        Line2.fromPoints(Vec2(1, 1), Vec2(10, 10)),
+        Line2.fromPoints(1, 1, 10, 10)
+      ].forEach(function(line) {
+        ok(line.slope() === 1);
+        ok(line.yintercept() === 0);
+      });
+    });
+  });
+
+  describe('#intersect', function() {
+    it('computes the intersection of normal lines', function() {
+      var l1 = Line2.fromPoints(0, 0, 10, 10);
+      var l2 = Line2.fromPoints(0, 10, 10, 0);
+
+      var v = l1.intersect(l2);
+      ok(v.equal(5, 5));
+    });
+
+    it('returns false when lines are parallel', function() {
+      var l1 = Line2.fromPoints(0, 0, 10, 10);
+      var l2 = Line2.fromPoints(5, 5, 50, 50);
+
+      ok(!l1.intersect(l2));
+    });
+
+    it('finds the intersection of perpendicular lines (vertical-vertical)', function() {
+      var l1 = Line2.fromPoints(0, 0, 10, 0);
+      var l2 = Line2.fromPoints(5, 10, 5, 0);
+      ok(l1.intersect(l2).equal(5, 0));
+      ok(l2.intersect(l1).equal(5, 0));
+    });
+
+    it('finds the intersection of lines (vertial-diagonal)', function() {
+      var l1 = Line2.fromPoints(0, 0, 55, 25);
+      var l2 = Line2.fromPoints(30, 30, 30, 0);
+      ok(l1.intersect(l2).equal(30, 13.6363635));
+      ok(l2.intersect(l1).equal(30, 13.6363635));
+
+    });
+  });
+
+  describe('#isHorizontal', function() {
+    it('true if horizontal', function() {
+      var l = Line2.fromPoints(0, 0, 100, 0);
+      ok(l.isHorizontal());
+    });
+    it('false if not horizontal', function() {
+      var l = Line2.fromPoints(0, 0, 100, 10);
+      ok(!l.isHorizontal());
+    });
+  });
+
+  describe('#isVertical', function() {
+    it('true if vertical', function() {
+      var l = Line2.fromPoints(100, 0, 100, 100);
+      ok(l.isVertical());
+    });
+    it('false if not vertical', function() {
+      var l = Line2.fromPoints(10, 0, 100, 10);
+      ok(!l.isVertical());
+    });
+  });
+
+  describe('#closestPointTo', function() {
+    it('finds the closest point to this line (horizontal)', function() {
+      var l = Line2.fromPoints(0, 0, 100, 0);
+      ok(l.closestPointTo(Vec2(10, 10)).equal(Vec2(10, 0)));
+    });
+
+    it('finds the closest point to this line (vertical)', function() {
+      var l = Line2.fromPoints(100, 0, 100, 100);
+      ok(l.closestPointTo(Vec2(10, 10)).equal(Vec2(100, 10)));
+    });
+
+    it('finds the closest point to this line (diagonal)', function() {
+      var l = Line2.fromPoints(0, 0, 100, 100);
+      ok(l.closestPointTo(Vec2(25, 75)).equal(Vec2(50, 50)));
     });
   });
 });
